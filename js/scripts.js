ef0b1e74903fd65c85c088a75226347166bcd06b
jk <kerntzj@gmail.com>
2019-04-05 10:15:31 -0700

    add second spec to beepBoop function

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,11 @@ function beepBoop(userNumberInput){
   for(var i = 0; i <= userNumberInput; i++){
     userInputNumbers.push(i);
   }
+  for(var i = 0; i <= userInputNumbers.length - 1; i++){
+    if(userInputNumbers[i] === 3){
+      userInputNumbers.splice(i,1,"I'm sorry Dave, I'm afraid I can't do that.")
+    }
+  }
 
   $("#result").append(userInputNumbers.join(','));
 }
