7fdcb5cdbaf3004d5de3963d599ba6ace3dafc75
jk <kerntzj@gmail.com>
2019-04-07 07:10:56 -0700

    refactor scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,17 @@ $(document).ready(function()  {
     var userNumberInput = parseInt($("#userNumberInput").val());
     beepBoop(userNumberInput);
   })
-
 function beepBoop(userNumberInput){
   $("h4").empty()
+  if(userNumberInput < 0){
+      $("#result").append("Please insert positive value");
+  }
   var userInputNumbers = [];
   for(var i = 0; i <= userNumberInput; i++){
     userInputNumbers.push(i);
   };
   for(var i = 0; i <= userInputNumbers.length - 1; i++){
-    if(userInputNumbers[i] >= 10){
+    if(userInputNumbers[i] >= 10 || userInputNumbers.includes(3) ){
       var userInputString = userInputNumbers[i].toString();
         if(userInputString.includes("3")){
           userInputNumbers.splice(i,1,"I'm sorry Dave, I'm afraid I can't do that")
@@ -21,16 +23,6 @@ function beepBoop(userNumberInput){
         }else if (userInputString.includes("1")) {
           userInputNumbers.splice(i,1,"Beep!")
         }
-    }
-    if(userInputNumbers.includes(3)){
-      var sorryDave = userInputNumbers.indexOf(3)
-      userInputNumbers.splice(sorryDave,1,"I'm sorry Dave, I'm afraid I can't do that")
-    }else if (userInputNumbers.includes(2)) {
-      var boop = userInputNumbers.indexOf(2)
-      userInputNumbers.splice(boop,1,"Boop")
-    }else if (userInputNumbers.includes(1)) {
-      var beep = userInputNumbers.indexOf(1)
-      userInputNumbers.splice(beep,1,"Beep!")
     }
   }
   $("#result").append(userInputNumbers.join(', '));
